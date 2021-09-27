2158e2336775f3e6bd87d5c1004ba8ba45f43ced
TattaS <tatiana.shilontseva@yandex.ru>
2021-09-27 20:42:28 +0300

    fast_game

--- project_0/fast_game.py
+++ project_0/fast_game.py
@@ -3,34 +3,34 @@
 """
 import numpy as np
 
-def fast_random_predict(n_number:int=1):
+def fast_predict(num:int=1):
     """Угадываем число по алгоритму
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        num (int, optional): Загаданное число. Defaults to 1.
 
     Returns:
         int: Число попыток
     """
-    count = 0
-    min_number = 0
-    max_number = 101
+    count = 0 # счетчик попыток
+    min_num = 0 # нижняя граница диапазона чисел для рандомного выбора
+    max_num = 101 # верхняя граница диапазона чисел для рандомного выбора
     while True:
         count += 1
-        predict_number = (max_number - min_number)//2
-        if predict_number == n_number:
+        predict_number = np.random.randint(min_num, max_num) # предполагаемое число
+        if predict_number == num:
             break
-        elif predict_number < n_number:
-            max_number == predict_number
+        elif predict_number < num:
+            min_num = predict_number
         else:
-            min_number == predict_number
+            max_num = predict_number
     return count
 
-def fast_score_game(fast_random_predict) -> int:
+def fast_score_game(fast_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов наш алгоритм угадывает число
 
     Args:
-        fast_random_predict ([type]): функция угадывания
+        fast_predict (num): функция угадывания
 
     Returns:
         int: среднее количество попыток
@@ -39,11 +39,11 @@ def fast_score_game(fast_random_predict) -> int:
     np.random.seed(1) # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
     
-    score_ls = list(map(fast_random_predict, random_array))
+    score_ls = list(map(fast_predict, random_array))
     score = int(np.mean(score_ls)) 
     
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return score
 
 if __name__=='__main__':
-    fast_score_game(fast_random_predict)      
+    fast_score_game(fast_predict)      
